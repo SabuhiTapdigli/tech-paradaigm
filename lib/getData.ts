883d3e35cef9fc1833a1db30b93161db9beca7d7
SabuhiTapdigli <sabuhitapdigli@gmail.com>
2023-12-04 14:26:19 +0400

    new api

--- lib/getData.ts
+++ lib/getData.ts
@@ -2,8 +2,7 @@ import { headers } from "next/headers";
 
 const useFetch = async (gclid: any) => {
   const ip = headers().get("x-forwarded-for");
-  console.log("ip", ip);
-  console.log("gclid", gclid);
+
   const url = `https://api.gameindustrytitans.com/logic`;
 
   const res = await fetch(url, {
@@ -12,7 +11,6 @@ const useFetch = async (gclid: any) => {
     body: JSON.stringify({ address: ip, gclid: gclid }),
   });
   const data = await res.json();
-  console.log("data", data);
   return data;
 };
 
